a7bd813120c2be719706292b7262c3e1118966d0
JADL2023 <loco1914oo@gmail.com>
2023-06-29 15:21:54 -0600

    Se agrega tablas

---
+++ Unidad2/2-6for tablas.java
@@ -0,0 +1,25 @@
+
+package pkg2.pkg6fortablas;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        int i,j;
+        for(i=1; i<=10; i++){
+        System.out.println("Tabla del " + i); 
+        System.out.println("-----------------");
+        
+        for(j=1; j<=10; j++){
+            System.out.println(i + "X" + j + " = " + j*i);
+            
+        }
+    }
+        
+            
+        
+    }
+    
+}
